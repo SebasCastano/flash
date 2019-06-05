9d747fea92163402beeb6aa26caa52c644712a3e
sebascastano <castano.sebastian@correounivalle.edu.co>
2019-06-05 17:13:31 -0500

    Archivos base del sistema

---
+++ apps/usuario/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class Usuario(AbstractUser):
+
+    CARGOS_USUARIO_CHOICES = (
+        ('Secretaria', 'Secretaria'),
+        ('Operador', 'Operador'),
+        ('Contador', 'Contador'),
+        ('Gerente', 'Gerente'),
+        ('Auxiliar operacion', 'Auxiliar operacion')
+    )
+    cargo = models.CharField(max_length=30, choices=CARGOS_USUARIO_CHOICES)
+    direccion = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=12, blank=True)
+    celular = models.CharField(max_length=12, blank=True)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        empleado = self.username + ' - ' + self.first_name + ' ' + self.last_name
+        return empleado
+
+    def get_cargo(self):
+        return self.cargo
